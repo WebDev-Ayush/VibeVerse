18813d5435a0ff514342d6f43edad2a46636fe02
Ayush Bisht <ayush.bisht.cseds.2022@miet.ac.in>
2025-01-27 20:30:10 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# VibeVerse
+ 
---
+++ next.config.mjs
@@ -0,0 +1,27 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    staleTimes: {
+      dynamic: 30,
+    },
+  },
+  serverExternalPackages: ["@node-rs/argon2"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+  rewrites: () => {
+    return [
+      {
+        source: "/hashtag/:tag",
+        destination: "/search?q=%23:tag",
+      },
+    ];
+  },
+};
+
+export default nextConfig;
